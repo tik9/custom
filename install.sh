634145236ab1f8f38f8033adfbe2890e6ab86e04
timokoerner <cow>
2016-06-10 10:19:39 +0200

    sh's zum installieren und starten von Apps hinzu

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+home="~/";
+
+echo "Home ist $home"
+#sudo add-apt-repository ppa:webupd8team/java
+#sudo apt-get update
+
+
+# apt-get
+apt-get install -y 
+
+# Java
+# aptget=("oracle-java8-installer" ant 'tomcat8' )
+
+# Python
+  #"ipython3" "python3-pip"
+
+# Editoren
+#"geany" "vim"
+
+# Tex
+# http://tex.stackexchange.com/questions/65762/how-to-install-additional-latex-packages-in-debian-ubuntu
+# texlive-latex-base
+
+#Browser
+#"chromium-browser" links2
+
+# Dict
+# dict dict-de-en dictd
+
+# Server
+# git mysql-server libmysqlclient-dev openssh-server
+
+# Mutt
+# mutt msmtp
+
+# musik
+# "moc" mplayer ffmpeg
+
+# Sonstige Programme
+# mupdf ack-grep imagemagick tree
+
+#	pip install --upgrade "$i"
+
+
+echo "${0} fertig"
---
+++ startRoot.sh
@@ -0,0 +1,18 @@
+exec zsh
+
+#pycharm
+#py
+
+#eclipse
+#e
+
+# Aliase in geany
+
+#te
+#te
+
+# ifup down up wlan
+#i2
+
+da
+echo Fertig $0
