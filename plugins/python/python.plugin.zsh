15fc848ddd8b46cf8a4d70262ae126eff45f1576
lubuntu <ubuntu schwarz>
2017-12-20 12:03:30 +0100

    plugin-django-python-hinzu

---
+++ plugins/python/python.plugin.zsh
@@ -0,0 +1,37 @@
+
+# Entferne python kompilierter byte-code im aktuellen Verzeichnis oder in einer Verzeichnisliste
+function pyclean() {
+    ZSH_PYCLEAN_PLACES=${*:-'.'}
+    find ${ZSH_PYCLEAN_PLACES} -type f -name "*.py[co]" -delete
+    find ${ZSH_PYCLEAN_PLACES} -type d -name "__pycache__" -delete
+}
+
+
+# Finde python Dateien
+alias pyfind='find . -name "*.py"'
+
+
+# Grep
+alias pygrep='grep --include="*.py"'
+
+function p3(){
+	if [ $os != "CYGWIN_NT-6.1" ]; then
+
+		python3 $@
+	else
+		python $@;fi
+}
+
+alias kid="kill -9 $(netstat -lnp | grep 8000 | awk '{print $7}' | cut -d"/" -f 1)"
+alias py='/root/progr/pycharm/bin/pycharm.sh&'
+alias pb="b $pb"
+alias pp='pr python' # grep ps nach python
+
+#pip
+alias pdn='pipdeptree --package'
+alias pdr='pipdeptree -r --package'
+alias pi='p3 -m pip install'
+alias pui='pip3 uninstall -y'
+alias pis='p3 -m pip show'
+alias ppl='p3 -m pip list n2'
+alias req='pi -r requirements.txt'
