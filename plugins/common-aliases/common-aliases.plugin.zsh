7b872a7978a301acea2e34a163867980e4697d5e
root <fujitsu lubu>
2020-09-03 07:54:40 +0200

    fork-sh-fuer-linkedintest

--- plugins/common-aliases/common-aliases.plugin.zsh
+++ plugins/common-aliases/common-aliases.plugin.zsh
@@ -17,7 +17,7 @@ alias x=exit
 #cds
 alias cv='/home/t/cv'
 alias ml='/root/ml'
-
+alias lt='~/lt'
 
 alias o=$ZSH_CUSTOM
 alias pm='sudo pm-suspend'
@@ -76,7 +76,6 @@ alias l='ls -lFh'     #size,show type,human readable
 alias la='ls -lAFh'   #long list,show almost all,show type,human readable
 alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
 alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
-alias ll='ls -l'      #long list
 alias ldot='ls -ld .*'
 alias lS='ls -1FSsh'
 alias lart='ls -1Fcart'
