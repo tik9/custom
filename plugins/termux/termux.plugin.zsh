da9787739de60f1c2cf5cc19d4acb5a3d8e8ee79
ti ko <thome@tis-Mac>
2022-12-28 12:23:20 +0100

    now-with-termux-plugin

---
+++ plugins/termux/termux.plugin.zsh
@@ -0,0 +1,10 @@
+
+# apt
+alias aar='apt autoremove -y'
+alias ad="apt update"
+alias ai="apt list --installed | grep"
+alias al='apt list --upgradable'
+alias ar="apt remove"
+alias as='apt show'
+alias au="apt upgrade"
+alias in='apt install -y'
